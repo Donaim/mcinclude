9333e109d802fd2f82aaeac38eea921e28d888f5
Donaim <0@kek.com>
2018-03-17 00:17:53 +0100

    +output secify

--- scripts/runtest.py
+++ scripts/runtest.py
@@ -6,18 +6,25 @@ project_dir = path.normpath(path.join(my_dir, '..'))
 test_dir = path.join(project_dir, 'test', 'units')
 src_dir = path.join(project_dir, 'src')
 
+includes = [src_dir, os.path.join(project_dir, 'extern', 'doctest', 'doctest')]
+includes = list(map(lambda p: '-I' + p, includes))
+
 def compile_run(main_source):
+    # compile /src
+    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++no-link', '++exclude', 'main.cpp', '++copts'] + includes) # do not compile main.cpp
 
-    includes = [src_dir, os.path.join(project_dir, 'extern', 'doctest', 'doctest')]
-    includes = list(map(lambda p: '-I' + p, includes))
-    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++no-link', '++exclude', 'main.cpp', '++copts'] + includes) # compile only, but not main.cpp
-
-    mybuild_path = path.normpath(path.join(test_dir, '..', 'build'))
-    if not path.exists(mybuild_path): os.makedirs(mybuild_path)
-    my_object_path = path.join(mybuild_path, path.basename(main_source) + '.o')
+    # compile this test file
+    my_build_dir = path.normpath(path.join(test_dir, '..', 'build'))
+    if not path.exists(my_build_dir): os.makedirs(my_build_dir)
+    my_object_path = path.join(my_build_dir, path.basename(main_source) + '.o')
     subprocess.check_call(['g++', '-c', main_source, '-o', my_object_path] + includes) # compile only this file
 
-    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++autorun', '++no-compile', '++lopts', my_object_path, '++exclude', 'main.o']) # link only, but not main.o
+    # link this test with /src
+    out_exe_path = path.join(my_build_dir, 'exe.exe')
+    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++no-compile', '++lopts', my_object_path, '++exclude', 'main.o', '++out', out_exe_path]) # do not include main.o
+    
+    # run
+    subprocess.call([out_exe_path])
 
 if __name__ == "__main__":
     target_argument = sys.argv[1]
