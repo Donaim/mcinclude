1ddeb1fbfc56c4b1aaff6dc649c5550e056ec986
Donaim <0@kek.com>
2018-03-16 23:39:42 +0100

    better testing

--- scripts/runtest.py
+++ scripts/runtest.py
@@ -7,12 +7,15 @@ test_dir = path.join(project_dir, 'test', 'units')
 src_dir = path.join(project_dir, 'src')
 
 def compile_run(main_source):
-    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++no-link', '++exclude', 'main.cpp']) # compile only, but not main.cpp
+
+    includes = [src_dir, os.path.join(project_dir, 'extern', 'doctest', 'doctest')]
+    includes = list(map(lambda p: '-I' + p, includes))
+    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++no-link', '++exclude', 'main.cpp', '++copts'] + includes) # compile only, but not main.cpp
 
     mybuild_path = path.normpath(path.join(main_source, '..', 'build'))
     if not path.exists(mybuild_path): os.makedirs(mybuild_path)
     my_object_path = path.join(mybuild_path, path.basename(main_source) + '.o')
-    subprocess.check_call(['g++', '-c', main_source, '-o', my_object_path]) # compile only this file
+    subprocess.check_call(['g++', '-c', main_source, '-o', my_object_path] + includes) # compile only this file
 
     subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++autorun', '++no-compile', '++lopts', my_object_path, '++exclude', 'main.o']) # link only, but not main.o
 
