c322898cb5f94ddeb78483070b8c059d9d8663c7
Donaim <0@kek.com>
2018-03-16 23:30:12 +0100

    better runtest

--- scripts/runtest.py
+++ scripts/runtest.py
@@ -1,47 +1,34 @@
 import subprocess, os, sys
+from os import path
 
-my_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-project_dir = os.path.abspath(os.path.join(my_dir, '..'))
-test_dir = os.path.join(project_dir, 'test')
+my_dir = path.normpath(path.dirname(path.abspath(sys.argv[0])))
+project_dir = path.normpath(path.join(my_dir, '..'))
+test_dir = path.join(project_dir, 'test', 'units')
+src_dir = path.join(project_dir, 'src')
 
-def compile_run(files):
-    if not files[0].endswith('.cpp'): files[0] += '.cpp'
-    test_file_name = os.path.basename(files[0])
+def compile_run(main_source):
+    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++no-link', '++exclude', 'main.cpp']) # compile only, but not main.cpp
 
-    includes = [os.path.join(project_dir, 'src'), os.path.join(project_dir, 'extern', 'doctest', 'doctest')]
-    includes = list(map(lambda p: '-I' + p, includes))
-    output_bin_path = os.path.abspath(os.path.join(test_dir, 'out-{}.exe'.format(test_file_name)))
-    options = ['-std=c++14']
+    mybuild_path = path.normpath(path.join(main_source, '..', 'build'))
+    if not path.exists(mybuild_path): os.makedirs(mybuild_path)
+    my_object_path = path.join(mybuild_path, path.basename(main_source) + '.o')
+    subprocess.check_call(['g++', '-c', main_source, '-o', my_object_path]) # compile only this file
 
-    compile_args = ['g++'] + options + includes
-    compile_args += files
-    compile_args += ['-o', output_bin_path]
+    subprocess.check_call(['py', 'mxxbuild.py', src_dir, '++autorun', '++no-compile', '++lopts', my_object_path, '++exclude', 'main.o']) # link only, but not main.o
 
-    print('compiling:', ' '.join(compile_args), '', sep='\n')
-    subprocess.check_call(compile_args)
+if __name__ == "__main__":
+    target_argument = sys.argv[1]
 
-    print('running:', output_bin_path, '-' * len(output_bin_path), '', sep='\n')
-    subprocess.call(output_bin_path)
-def get_cpps():
-    import subprocess
-    proc = subprocess.Popen(['py', os.path.join(my_dir, 'get_cpps.py')], stdout=subprocess.PIPE)
-    proc.wait()
-    text = proc.stdout.read()
-    text = text.decode('utf-8')
-    lines = text.split(os.linesep)
-    lines = filter(lambda f: len(f) > 1 and not f.isspace() and os.path.basename(f) != 'main.cpp', lines)
+    if target_argument == 'all':
+        for f in os.listdir(test_dir):
+            if f.endswith('.cpp') and not f.startswith('_'): 
+                try: compile_run(path.join(test_dir, f))
+                except: pass
+    else:
+        target_test_file = path.join(test_dir, target_argument)
+        compile_run(target_test_file)
 
-    lines = map(lambda f: os.path.abspath(f), lines)
-    return list(lines)
+    print("END")
 
-cpps = get_cpps()
-if sys.argv[1] == 'all':
-    for f in os.listdir(test_dir):
-        if f.endswith('.cpp') and not f.startswith('_'): 
-            try: compile_run( [os.path.join(test_dir, f)] + cpps)
-            except: pass
-else:
-    target_test_file = os.path.abspath(os.path.join(test_dir, sys.argv[1]))
-    compile_run([target_test_file] + cpps)
 
 
--- test/units/_string_manip.cpp
+++ test/units/_string_manip.cpp
@@ -5,4 +5,8 @@ TEST_CASE("experimenting with strings") {
     string s2{s1};
 
     CHECK(strcmp(s1.c_str(), s2.c_str()) == 0);
+
+    s1.pop_back();
+
+    CHECK_FALSE(strcmp(s1.c_str(), s2.c_str()) == 0);
 }
