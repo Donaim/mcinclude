7d071e06010ca9d1fb3eabaf109b5d14841bf816
Donaim <0@kek.com>
2018-03-17 22:45:16 +0100

    + is_ended flag

--- src/sfile.cpp
+++ src/sfile.cpp
@@ -5,6 +5,7 @@ SFile::SFile(const char * path_, SFile * parent, LineReader& reader) :
         path(path_), // copies chars -> no safety issues
         reader_(reader),
         parent_file_(parent),
+        ended_(false),
         CodeBlock(parent != nullptr ? parent->parent_block() : nullptr) 
     {
     }
@@ -14,12 +15,14 @@ SFile SFile::create_root(const char * path) {
 }
 
 bool SFile::is_file_root() const { return parent_file_ == nullptr; }
+bool SFile::is_ended() const { return ended_; }
 
 void SFile::read_lines() {
     while (!reader_.is_end()) {
         MString& ms = MString::from_reader_line(reader_);
         lines.push_back(&ms);
     }
+    ended_ = true;
 }
 
 SFile::~SFile() {
--- src/sfile.h
+++ src/sfile.h
@@ -13,13 +13,15 @@ class SFile : public CodeBlock {
 protected:
     LineReader& reader_;
     SFile * const parent_file_;
+    bool ended_;
 public:
     const string path;
 
     SFile(const char * path_, SFile * parent, LineReader& reader);
     static SFile create_root(const char * path);
 
-    bool is_file_root() const ;
+    bool is_file_root() const;
+    bool is_ended() const;
 
     void read_lines();
 
