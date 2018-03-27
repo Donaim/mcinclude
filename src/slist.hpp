fbc0b6f572e841172a7ce7b7f9bf68b644d054c4
Donaim <0@kek.com>
2018-03-27 22:54:13 +0200

    improved slist

--- src/slist.hpp
+++ src/slist.hpp
@@ -7,35 +7,45 @@
 using std::runtime_error;
 
 /*
-    simple lists. no checks, no safety. only special-case use
-    do not have destructor!
-*/
+ *   simple lists. no checks, no safety 
+ *   used for better control
+ *   note: do not have destructor!
+ */
 
 template <typename T>
 class SArray {
-    const T * buffer;
+protected:
+    T * buffer;
     size_t _size;
+    SArray() {}
 public:
-    SArray(const T * source, size_t len) : buffer(source), _size(len)
+    SArray(const T * source, size_t len) : buffer((T*)source), _size(len)
     {}
     
-    inline T * source() const { return buffer; }
-    inline size_t size() const { return _size; }
-    inline bool is_empty() const { return _size == 0; }
+    inline T * source() const { return this->buffer; }
+    inline size_t size() const { return this->_size; }
+    inline bool is_empty() const { return this->_size == 0; }
     
     inline void dofree() { // uses c-style free(*)
-        free((T*)buffer);
-        _size = 0;
+        free((T*)this->buffer);
+        this->_size = 0;
     }
     inline void del() { // uses c++ style delete[]
-        delete[] (T*)buffer;
-        _size = 0;
+        delete[] (T*)this->buffer;
+        this->_size = 0;
+    }
+    inline void delete_targets() { // the array of pointers stays in memory, but targets are deallocated.
+        for (int i = 0; i < this->_size; i++) {
+            delete this->buffer[i];
+        }
+        this->_size = 0;
+        // dont change capacity
     }
 
-    inline T operator [] (std::size_t i) const { return buffer[i]; }
+    inline T operator [] (std::size_t i) const { return this->buffer[i]; }
     inline T at(size_t i) const {
-        if (i < _size) {
-            return buffer[i];
+        if (i < this->_size) {
+            return this->buffer[i];
         } else {
             throw std::runtime_error("index was outside of SArray!");
         }
@@ -43,9 +53,7 @@ public:
 };
 
 template <typename T>
-class SList { 
-    T * buffer;
-    size_t _size;
+class SList : public SArray<T> { 
     size_t _capacity;
 public:
     SList(size_t capac_init) {
@@ -56,25 +64,25 @@ public:
         if (capac_init <= 0) { capac_init = 1; }
    
         _capacity = capac_init;
-        buffer = (T*) malloc (sizeof(T) * _capacity);
-        _size = 0;
+        this->buffer = (T*) malloc (sizeof(T) * _capacity);
+        this->_size = 0;
     }
 
     inline void push_back(const T & v) {
-        if (_size >= _capacity) {
+        if (this->_size >= _capacity) {
             reserve(_capacity * 2);
         }
-        buffer [_size++] = v;
+        this->buffer [this->_size++] = v;
     }
     inline void push_back_copy(const T v) {
-        if (_size >= _capacity) {
+        if (this->_size >= _capacity) {
             reserve(_capacity * 2);
         }
-        buffer [_size++] = v;
+        this->buffer [this->_size++] = v;
     }
     inline void remove_last() {
-        if(_size > 0) {
-            _size--;
+        if(this->_size > 0) {
+            this->_size--;
         }
     }
 
@@ -82,41 +90,9 @@ public:
         if (capacity <= 0) { capacity = 1; }
 
         _capacity = capacity;
-        buffer = (T*)realloc(buffer, sizeof(T) * capacity);
-    }
-    inline T * source() const { return buffer; }
-    inline size_t size() const { return _size; }
-
-    inline void clear_dont_free() { _size = 0; }
-    inline void dofree() {
-        free(buffer);
-        _size = 0;
-        _capacity = 0;
-    }
-    inline void delete_targets() { // the array of pointers stays in memory, but targets are deallocated.
-        for (int i = 0; i < _size; i++) {
-            delete buffer[i];
-        }
-        _size = 0;
-        // dont change capacity
-    }
-    inline SArray<T> move_to_array() {
-        SArray<T> re(buffer, _size);
-
-        buffer = nullptr;
-        _capacity = 0;
-        _size = 0;
-
-        return re;
+        this->buffer = (T*)realloc(this->buffer, sizeof(T) * capacity);
     }
 
-    inline T operator [] (std::size_t i) const { return buffer[i]; }
-    inline T at(size_t i) {
-        if (i < _size) {
-            return buffer[i];
-        } else {
-            throw std::runtime_error("index was outside of SList!");
-        }
-    }
+    inline void clear_dont_free() { this->_size = 0; }
 };
 
