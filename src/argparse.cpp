21232dd9aab25ac12237c6c85871548cc8eac28f
Donaim <0@kek.com>
2018-03-29 00:18:23 +0200

    replaced vector with SList. (vector is unpredictable)

--- src/argparse.cpp
+++ src/argparse.cpp
@@ -34,7 +34,21 @@ SList<string *> split_source(string source) {
 
     return re;
 }
+SList<string *> split_mstring(const MString& source) {
+    SList<string *> re{4};
 
+    auto sp = source.split_into_raw();
+    for (int i = 0; i < sp.size(); i++) {
+        re.push_back_copy(new string{sp[i].source()});
+    }
+
+    return re;
+}
+ArgParse::ArgParse(const MString& source) 
+    : ArgParse(split_mstring(source)) 
+{
+    
+}
 ArgParse::ArgParse(string source) 
     : ArgParse(split_source(source)) 
 {
@@ -55,8 +69,8 @@ ArgParse::ArgParse(SList<string *> parts)
         }
         else {
             if (last_narg == nullptr) {
-                // DPLOG("NEWARG:%s", parts[i]->c_str());
-                args.push_back(string(*parts[i]));
+                DPLOG("NEWARG: [%s]", parts[i]->c_str());
+                args.push_back_copy(new string(*parts[i]));
             } else {
                 // DPLOG("NEW NARG:%s", parts[i]->c_str());
                 last_narg->push_arg(*parts[i]);
@@ -68,17 +82,24 @@ ArgParse::ArgParse(SList<string *> parts)
 string ArgParse::get_option(const string& name) const {
     for (int i = 0, to = args.size() - 1; i < to; i++) {
         // DPLOG("curr=[%s], next=[%s]", args[i].c_str(), args[i + 1].c_str());
-        if (args[i] == name) { return (args[i + 1]); }
+        if (*(args[i]) == name) { return *(args[i + 1]); }
     }
 
     return "";
 }
 string ArgParse::get_tag(const string & name ) const {
-    for (int i = 0, to = nargs.size(); i < to; i++) {
-        if (args[i] == name) { return (args[i]); }
+    for (int i = 0, to = args.size(); i < to; i++) {
+        if (*args[i] == name) { return *(args[i]); }
     }
 
     return "";
+}
+string ArgParse::get_tag_at(size_t index) const {
+    if (args.size() > index) {
+        return *(args[index]);
+    } else {
+        return "";
+    }
 }
 IArray<string *> ArgParse::get_nargs(const string & name ) const {
     for (int i = 0, to = nargs.size(); i < to; i++) {
@@ -92,7 +113,8 @@ IArray<string *> ArgParse::get_nargs(const string & name ) const {
 }
 
 ArgParse::~ArgParse() {
-    args.clear();
+    args.delete_targets();
+    args.dofree();
 
     nargs.delete_targets();
     nargs.dofree();
--- src/argparse.h
+++ src/argparse.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include "mstring.h"
+
 #include <string>
 #include <slist.hpp>
 #include <vector>
@@ -18,16 +20,18 @@ public:
 
 class ArgParse {
 public:
-    std::vector<std::string> args; // without nargs
+    SList<std::string*> args; // without nargs
     SList<NArg *> nargs;
     
     static const std::string NARG_SPLITCHAR;
 
     ArgParse(std::string source);
+    ArgParse(const MString& ms);
     ArgParse(SList<std::string*> parts);
 
     std::string get_option(const std::string& name) const; // like `verbose=1`
     std::string get_tag(const std::string& name) const; // bool
+    std::string get_tag_at(size_t index) const;
     IArray<std::string*> get_nargs(const std::string& name) const; // like `outs : a.c b.c c.c 
 
     ~ArgParse();
